77837300c3acf1a87f0ffb10fc20177d0f7cc20b
denulemos <denu.a.lemos@gmail.com>
2020-08-31 22:50:43 -0300

    cambios

--- Apuntes/destructuring.js
+++ Apuntes/destructuring.js
@@ -2,9 +2,9 @@ const user = {
 nombre: 'Denisse', edad: 22, ciudad:'BsAs'
 };
 
-const nombre = user.nombre //referenciamos directamente al objeto user
-console.log(nombre);
+const nombre2 = user.nombre //referenciamos directamente al objeto user
+console.log(nombre2);
 
 //Podemos tomar varias cosas a la vez de usuario, sin tener que referenciarlo como antes
 const {nombre, edad} = user;
-console.log(nombre)
+console.log(nombre2)
