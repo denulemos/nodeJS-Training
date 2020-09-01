4dd11362422b933155415073f5d28ece395ccf7b
Denisse Lemos <denu.a.lemos@gmail.com>
2020-08-31 23:09:24 -0300

    apuntes

---
+++ Apuntes/graphQL.js
@@ -0,0 +1,6 @@
+/* 
+GraphQL resuelve ciertos problemas de REST:
+-Es un solo endpoint para cualquier tipo de data que se quiera obtener y REST son varios endpoints dependiendo de lo que quiero
+-Instalar => npm -i graphql
+-Schema: Es el centro de cualquier servidor de graphQL, describe la funcionalidad disponible a los clientes que se encuentran conectados al mismo
+*/
